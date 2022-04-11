42ff51593dc13045e4ffc8e135c8b7874ffcde6e
Pedro Pacheco <pedropacheco1523503@gmail.com>
2022-04-10 20:21:29 -0600

    Proptypes por default

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,18 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const PrimeraApp = ({saludo}) => {
+const PrimeraApp = ({saludo, subtitulo}) => {
 
   return ( 
     <>
       <h1>{ saludo }</h1> 
-      <p>Mi primera aplicacion</p>
+      <p>{ subtitulo }</p>
     </>
   );
 }
-
+// Esto es una forma de documentar y solicitar a otros desarrolladores que envien los props cuando utilicen el componente 
 PrimeraApp.propTypes = {
   saludo: PropTypes.string.isRequired
 };
+
+// esto lo que hace es crear los props o asignarles un valor por defecto cuando no se envia nada.
+PrimeraApp.defaultProps = {
+  saludo: '',
+  subtitulo : 'Este es un proptype por default'
+}
  
 export default PrimeraApp;
