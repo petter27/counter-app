23fc4067b629c271c076779feefe0b4ca43c7c7e
Pedro Pacheco <pedropacheco1523503@gmail.com>
2022-04-21 02:12:51 -0600

    Agregando enzyme al proyecto para las pruebas
    Enzyme es una libreria de pruebas creada por Airbnb y ahora mantenida por facebook.
    
    Para instalar enzyme en versiones de react > 16 es necesario correr los siguientes comandos:
    
    npm i --save-dev enzyme enzyme-adapter-react-17 --legacy-peer-deps
    
    npm install --save-dev enzyme-to-json
    
    Tambien agregar la configuracion requerida al archivo setupTest.js

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
 
 describe('Pruebas en componente Primera App', ()=>{
-
+/*
   test('El saludo debe ser "Hola soy Goku"', ()=>{
 
     const saludo = 'Hola soy goku';
@@ -13,6 +14,14 @@ describe('Pruebas en componente Primera App', ()=>{
 
     // eslint-disable-next-line testing-library/prefer-screen-queries
     expect( getByText(saludo) ).toBeInTheDocument();
-  });
+  }); */
+  test('Debe de mostrar <PrimeraApp/> Correctamente', ()=>{
 
+    const saludo = 'Hola soy Goku';
+
+    const wrapper = shallow(<PrimeraApp saludo={ saludo } />);
+
+    expect( wrapper ).toMatchSnapshot();
+
+  });
 });
