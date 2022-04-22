bf09ce7075e3458217ac1f5d2734adfda35dd0ab
Pedro Pacheco <pedropacheco1523503@gmail.com>
2022-04-21 23:30:43 -0600

    Update CounterApp.test.js

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -20,7 +20,6 @@ describe('Pruebas para el componente CounterApp', ()=>{
   test('Debe mostrar el valor de 100 enviado como valor por default', ()=>{
 
     const valor = 100;
-
     const wrapper = shallow(<CounterApp value={ valor } />);
 
     expect(valor).toBe( parseInt(wrapper.find('h2').text().trim()));
